f873e9c9d7e202060e62b73e27e0b3dd94e293be
Andreas Fink <andreas.fink@cscs.ch>
2017-12-01 17:17:13 +0100

    *add* add exchangelib to the virtualenv (not yet used, but maybe in the future)

--- create_virtualenv.sh
+++ create_virtualenv.sh
@@ -7,7 +7,7 @@ export VENV=~/coding/pyhttpserver_env
 
 python3 -m venv $VENV
 $VENV/bin/pip install --upgrade pip
-$VENV/bin/pip install pyramid requests lxml cssselect dbus-python flask flask-socketio flask-bootstrap eventlet vdirsyncer pydal pyxdg vobject
+$VENV/bin/pip install pyramid requests lxml cssselect dbus-python flask flask-socketio flask-bootstrap eventlet vdirsyncer pydal pyxdg vobject exchangelib
 
 FROM=$(python3 -c "import gi; import os; print(os.path.dirname(gi.__file__))")
 TO=$($VENV/bin/python3 -c "import os; import cssselect; print(os.path.dirname(os.path.dirname(cssselect.__file__)))")

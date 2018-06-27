2bceadd6ab63ea97e764461e7a7020fc32706fec
Andreas Fink <finkandreas@web.de>
2018-06-27 06:55:47 +0200

    *change* restring log div to maximum 10 messages

--- static/newtab/socket.js
+++ static/newtab/socket.js
@@ -32,23 +32,34 @@ $(function() {
       $('#ping').html("Last websocket ping: " + new Date().toString());
     });
 
-    socket.on('connect', function() { $('#socket_info').append('<div>connect at ' + new Date().toString() + "</div>"); });
-    socket.on('connect_error', function(error) { $('#socket_info').append('<div>connect_error at ' + new Date().toString() + " error=" + error + "</div>"); });
-    socket.on('connect_timeout', function(timeout) { $('#socket_info').append('<div>connect_timeout at ' + new Date().toString() + " timeout=" + timeout + "</div>"); });
-    socket.on('error', function(error) { $('#socket_info').append('<div>error at ' + new Date().toString() + " error=" + error + "</div>"); });
-    socket.on('disconnect', function(reason) { $('#socket_info').append('<div>disconnect at ' + new Date().toString() + " reason=" + reason + "</div>"); });
-    socket.on('reconnect', function(attempt) { $('#socket_info').append('<div>reconnect at ' + new Date().toString() + " attempt=" + attempt + "</div>"); });
-    socket.on('reconnect_attempt', function(attempt) { $('#socket_info').append('<div>reconnect_attempt at ' + new Date().toString() + " attempt=" + attempt + "</div>"); });
-    socket.on('reconnecting', function(attempt) { $('#socket_info').append('<div>reconnecting at ' + new Date().toString() + " attempt=" + attempt + "</div>"); });
-    socket.on('reconnect_error', function(error) { $('#socket_info').append('<div>reconnect_error at ' + new Date().toString() + " error=" + error + "</div>"); });
-    socket.on('reconnect_failed', function() { $('#socket_info').append('<div>reconnect_failed at ' + new Date().toString() + "</div>"); });
+    socket.on('connect', function() { console.log("socket connect"); });
+    socket.on('connect_error', function(error) { console.log("socket connect_error. error="+error); });
+    socket.on('connect_timeout', function(timeout) { console.log("socket connect_timeout. timeout="+timeout); });
+    socket.on('error', function(error) { console.log("socket error. error="+error); });
+    socket.on('disconnect', function(reason) { console.log("socket disconnect. reason="+reason); });
+    socket.on('reconnect', function(attempt) { console.log("socket reconnect. attempt="+attempt); });
+    socket.on('reconnect_attempt', function(attempt) { console.log('socket reconnect_attempt. attempt='+attempt); });
+    socket.on('reconnecting', function(attempt) { console.log("socket reconnecting. attempt="+attempt); });
+    socket.on('reconnect_error', function(error) { console.log('socket reconnect_error. error='+error); });
+    socket.on('reconnect_failed', function() { console.log('socket reconnect_failed'); });
   }, 20*1000)
 
   console.log("Overriding console object");
   orig_log = console.log;
   orig_warn = console.warn;
   orig_error = console.error;
-  console.log   = function() { orig_log.apply(this, arguments);   $('#socket_info').append('<div>Log: '+  JSON.stringify(arguments)+'</div>'); };
-  console.warn  = function() { orig_warn.apply(this, arguments);  $('#socket_info').append('<div>Warn: '+ JSON.stringify(arguments)+'</div>'); };
-  console.error = function() { orig_error.apply(this, arguments); $('#socket_info').append('<div>Error: '+JSON.stringify(arguments)+'</div>'); };
+  var consoleDivAdd = function(msg) {
+    var now = new Date();
+    var twoDigit = function(nbr) {
+      if (nbr<10) return '0'+nbr;
+      else return ''+nbr;
+    }
+    DateString = '' + twoDigit(now.getDate()) + "." + twoDigit(now.getMonth()) + " " + twoDigit(now.getHours()) + ":" + twoDigit(now.getMinutes());
+    $('#socket_info').append('<div />').html(DateString + ":" + msg);
+    allChildren = $('#socket_info').children();
+    for (var i=0; i<allChildren.length-10; ++i) allChildren[i].remove();
+  };
+  console.log   = function() { orig_log.apply(this, arguments);   consoleDivAdd('Log: '+  JSON.stringify(arguments)); };
+  console.warn  = function() { orig_warn.apply(this, arguments);  consoleDivAdd('Warn: '+ JSON.stringify(arguments)); };
+  console.error = function() { orig_error.apply(this, arguments); consoleDivAdd('Error: '+JSON.stringify(arguments)); };
 });
